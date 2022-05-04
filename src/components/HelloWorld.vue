4f75608288197dca5060e0df09d8a31ab01dbd6a
romain_dewil <romain.dewil@instant-system.com>
2022-05-04 15:57:11 +0200

    Fix main.scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -46,13 +46,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
 body {
   margin: 0;
   padding: 0;
   font-family: Arial, sans-serif;
   color: white;
-  background-color: #202020;
 }
 a {
   color: white;
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/scss/main.scss";
+        `
+      }
+    }
+  }
+}
